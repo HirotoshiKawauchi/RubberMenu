baa13b0bb86292db2cd38c849d420ab58fc7ffbe
hirotoshi <hirotoshi_note_pc@yahoo.co.jp>
2017-04-21 23:44:06 +0900

    first commit

---
+++ RubberMenu/FlowLayout/RubberLayout.swift
@@ -0,0 +1,13 @@
+//
+//  RubberLayout.swift
+//  RubberMenu
+//
+//  Created by HIROTOSHI KAWAUCHI on 2017/04/09.
+//  Copyright © 2017年 HIROTOSHI KAWAUCHI. All rights reserved.
+//
+
+import UIKit
+
+class RubberLayout: UICollectionViewFlowLayout {
+
+}
